c115161c538f1e473870be2e793b84d87f92cf55
Nicolas Herrera <herreran903@gmail.com>
2024-11-04 17:19:18 -0500

    It started with the rabbit configuration

--- src/main/java/com/stock_service/stock/infra/brand/in/BrandController.java
+++ src/main/java/com/stock_service/stock/infra/brand/in/BrandController.java
@@ -2,7 +2,10 @@ package com.stock_service.stock.infra.brand.in;
 
 import com.stock_service.stock.app.brand.dto.BrandRequest;
 import com.stock_service.stock.app.brand.handler.IBrandHandler;
+//import com.stock_service.stock.infra.rabbit.MessageSender;
 import jakarta.validation.Valid;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -16,6 +19,9 @@ public class BrandController {
 
     private final IBrandHandler brandHandler;
 
+//    @Autowired
+//    private MessageSender messageSender;
+
     public BrandController(IBrandHandler brandHandler) {
         this.brandHandler = brandHandler;
     }
@@ -23,6 +29,12 @@ public class BrandController {
     @PostMapping(CREATE_BRAND_URL)
     ResponseEntity<Void> createBrand(@Valid @RequestBody BrandRequest brandRequest) {
         brandHandler.createBrand(brandRequest);
+        //messageSender.sendMessage(brandRequest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
+//    @RabbitListener(queues = "rollback_queue")
+//    public void rollbackPurchase() {
+//        System.out.println("Rolling back purchase of ");
+//    }
 }
